3a8de112bbf4c106dc250612724f593448150987
yeohchiheng <112604986+yeohchiheng@users.noreply.github.com>
2022-09-20 00:20:03 +0800

    Update EmpApp.py

--- EmpApp.py
+++ EmpApp.py
@@ -21,9 +21,9 @@ output = {}
 table = 'employee'
 
 
-@app.route("/", methods=['GET', 'POST'])
+@app.route("/getemp", methods=['GET', 'POST'])
 def home():
-    return render_template('AddEmp.html')
+    return render_template('GetEmp.html')
 
 
 @app.route("/about", methods=['POST'])
